4de022eb34f59c19759635fe74c38553c5f2bb93
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-04-15 05:29:09 -0400

    challenge de promsises parte 3: uso de put

--- src/promises/challenge.js
+++ src/promises/challenge.js
@@ -57,3 +57,25 @@ const data = {
 postData(`${API}/products`, data)
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+function putData(urlApi, dataUpdate) {
+  const response = fetch(urlApi, {
+    method: "PUT",
+    mode: "cors",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataUpdate),
+  });
+  return response;
+}
+
+const dataUpdate = {
+  title: "Se puede cambiar tambien otras caracteristicas",
+  price: 10, // no es necesario colocar todas las características del objeto, solo las que se cambiarán
+};
+
+putData(`${API}/products/271`, dataUpdate) //se debe colocar el id del objeto que se quiere modificar
+  .then((response) => response.json())
+  .then((dataUpdate) => console.log(dataUpdate));
