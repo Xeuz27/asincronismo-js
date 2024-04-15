a81437044c312e11cc00b8544cd0c78df9bb94f5
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-04-15 05:27:56 -0400

    challenge de promsises parte 2: uso de post

--- src/promises/challenge.js
+++ src/promises/challenge.js
@@ -33,3 +33,27 @@ fetchData(`${API}/products`)
   })
   .catch((err) => console.log(err)) //detectar un error
   .finally(() => console.log("Finally")); //es opcional para mostrar que se terminó la solicitud
+
+function postData(urlApi, data) {
+  const response = fetch(urlApi, {
+    method: "POST", //tiene que ir en mayúscula
+    mode: "cors", //cors es el permiso que va a tener, por defecto va estar siempre en cors
+    credentials: "same-origin", //es opcional
+    headers: {
+      "Content-Type": "application/json", //necesario indicar que lo que se está enviando es de tipo json
+    },
+    body: JSON.stringify(data), //el método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON
+  });
+  return response;
+}
+const data = {
+  title: "Nunca pares de aprender",
+  price: 2,
+  description: "A description",
+  categoryId: 1,
+  images: ["https://placeimg.com/640/480/any"],
+};
+//podemos usar el postData como una promesa y con .then obtener la respuesta como un objeto json y mostrarlo después en la consola
+postData(`${API}/products`, data)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
